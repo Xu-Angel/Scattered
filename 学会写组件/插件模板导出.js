ea93de700f280fa466c09fc225b92e7c067012e8
Angel <1049547382@qq.com>
2018-09-11 09:08:49 +0800

    add 写组件

--- 学会写组件/插件模板导出.js
+++ 学会写组件/插件模板导出.js
@@ -57,3 +57,47 @@
   }
 
 }());
+
+
+
+/*!todo:函数闭包写法 ！tddo: jQuery时期流行s;*/
+var TextCount = (function(){
+  //私有方法，外面将访问不到
+  var _bind = function(that){
+    that.input.on('keyup',function(){
+      that.render();
+    });
+  }
+
+  var _getNum = function(that){
+    return that.input.val().length;
+  }
+
+  var TextCountFun = function(config){
+
+  }
+
+  TextCountFun.prototype.init = function(config) {
+    this.input = $(config.id);
+    _bind(this);
+
+    return this;
+  };
+
+  TextCountFun.prototype.render = function() {
+    var num = _getNum(this);
+
+    if ($('#J_input_count').length == 0) {
+      this.input.after('<span id="J_input_count"></span>');
+    };
+
+    $('#J_input_count').html(num+'个字');
+  };
+  //返回构造函数
+  return TextCountFun;
+
+})();
+
+$(function() {
+  new TextCount().init({id:'#J_input'}).render();
+})
