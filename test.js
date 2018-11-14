f1b6cbe49eb74c33599d0699eeb6192876feebef
Angel <1049547382@qq.com>
2018-11-14 21:01:08 +0800

    netan

--- test.js
+++ test.js
@@ -5,3 +5,11 @@ console.log(i)
 }else{
   console.log(i,'555')
 }}
+
+const yObj = {y: 7, tt: 88, ii: 99}
+const mObj = {...yObj}  
+console.log(mObj)  // 全部拿来
+const { y, ...jObj} = yObj
+console.log(jObj)   // 拿指定的一部分
+[1,2].includes(1)
+Array.includes()
