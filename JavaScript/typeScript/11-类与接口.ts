419732280eb80ab522624b1d0a44b96541591a88
Xu-Angel <1049547382@qq.com>
2019-07-03 22:07:23 +0800

    update TS

--- JavaScript/typeScript/11-类与接口.ts
+++ JavaScript/typeScript/11-类与接口.ts
@@ -42,3 +42,81 @@ class Point {
 interface Point3d extends Point {
   z: number;
 }
+
+interface IInfo {
+  firstName: string
+  lastName: string
+}
+
+const getFullName = ({ firstName, lastName }: IInfo): string => {
+  return `${firstName} ${lastName}`
+}
+
+
+interface IVegetables {
+  type: string
+  color?: string
+}
+
+// 索引类型
+interface IRoleDic {
+  [id: number]: string
+}
+const role1: IRoleDic = {
+  0: 'spuer',
+  1: 'admin'
+}
+const role2: IRoleDic = ['super', 'admin']
+console.log(role1, role2)
+
+// readonly
+interface IReadonly {
+  readonly [id: number]: string
+}
+const ronly: IReadonly = {
+  0: 'super'
+}
+
+// ronly[0] = 'admin' // 报错
+
+// extends
+
+interface IAnimate {
+  age: number
+}
+
+interface IHuman {
+  sex: string
+}
+
+interface ISuperman extends IAnimate, IHuman {
+  skill: string
+}
+
+const superman1: ISuperman = {
+  age: 300,
+  sex: 'man',
+  skill: 'fire'
+}
+
+console.log(superman1)
+
+// 混合类型接口
+interface ICounter {
+  count: number
+  (): void
+}
+
+const getCounter = (): ICounter => {
+  const c = () => {
+    c.count++
+  }
+  c.count = 0
+  return c
+}
+
+const counter: ICounter = getCounter()
+counter()
+console.log(counter.count)
+counter()
+console.log(counter.count)
