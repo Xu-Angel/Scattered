ea1913ece3c68a6cf84f597536634f726f88a5c6
Angel <1049547382@qq.com>
2019-10-31 16:58:36 +0800

    learn Flutter

--- Dart&Flutter/shoppingCart.dart
+++ Dart&Flutter/shoppingCart.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 class Item {
   double price;
   String name;
@@ -11,7 +14,7 @@ class ShoppingCart {
   String name;
   DateTime date;
   String code;
-  List < Item > bookings;
+  List<Item> bookings;
 
   price() {
     double sum = 0.0;
@@ -27,9 +30,39 @@ class ShoppingCart {
     this.date = DateTime.now();
   }
 
-
   getInfo() {
-    return '购物车信息:' + '\n-----------------------------' + '\n用户名: ' + name + '\n优惠码: ' + code + '\n总价: ' + price().toString() + '\n日期: ' + date.toString() + '\n-----------------------------';
+    return '购物车信息:' +
+        '\n-----------------------------' +
+        '\n用户名: ' +
+        name +
+        '\n优惠码: ' +
+        code +
+        '\n总价: ' +
+        price().toString() +
+        '\n日期: ' +
+        date.toString() +
+        '\n-----------------------------';
+  }
+}
+
+get() async {
+  //创建网络调用示例，设置通用请求行为(超时时间)
+  var httpClient = HttpClient();
+  httpClient.idleTimeout = Duration(seconds: 5);
+
+  //构造URI，设置user-agent为"Custom-UA"
+  var uri = Uri.parse("https://flutter.dev");
+  var request = await httpClient.getUrl(uri);
+  request.headers.add("user-agent", "Custom-UA");
+
+  //发起请求，等待响应
+  var response = await request.close();
+
+  //收到响应，打印结果
+  if (response.statusCode == HttpStatus.ok) {
+    print(await response.transform(utf8.decoder).join());
+  } else {
+    print('Error: \nHttp status ${response.statusCode}');
   }
 }
 
@@ -37,4 +70,5 @@ void main() {
   ShoppingCart sc = ShoppingCart('小李子', '123456');
   sc.bookings = [Item('苹果', 10.0), Item('鸭梨', 20.0)];
   print(sc.getInfo());
+  get();
 }
